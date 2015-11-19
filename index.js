6517830dc4feff5f8fbe173b046fc04d49acebc1
Gean <w4ch4p@gmail.com>
2015-11-19 10:30:58 -0500

    create task styledown

---
+++ index.js
@@ -0,0 +1,45 @@
+var elixir          = require('laravel-elixir');
+var gulp            = require('gulp');
+var styledown 		= require('gulp-styledown');
+var notify          = require('gulp-notify');
+var _               = require('underscore');
+
+var Task = elixir.Task;
+
+/*
+ |----------------------------------------------------------------
+ | Gulp Styledown
+ |----------------------------------------------------------------
+ |
+ | This task will generate CSS styleguides.
+ | You can pass your custom options.
+ |
+ */
+
+elixir.extend('styledown', function (options) {
+
+    options = _.extend({
+		src     : '/path/to/styledown/*.md',
+		config  : '/path/to/config.md', // Path to config.md
+		filename: 'output.html', // Path to output html
+		dest    : 'paht/to/'
+    }, options);
+
+    var gulp_src = options.src;
+    var gulp_dest = options.dest;
+
+	var styledown_options = options;
+
+    new Task('styledown', function() {
+		return gulp.src(gulp_src)
+			.pipe(styldown(styledown_options))
+			.pipe.dest(gulp_dest)
+			.pipe(notify({
+				title: 'Styledown completed',
+				message: '<%= file.relative %> have been compiled.',
+				icon: __dirname + '/../laravel-elixir/icons/pass.png'
+			}));
+    })
+    .watch([ options.src ]);
+
+});
